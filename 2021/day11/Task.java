ee4bc08488d29a4dd31f0052f8f92db707d7f301
Ágocsi-Kiss Bence <akbence007@gmail.com>
2021-12-12 10:32:31 +0100

    2021 day11

--- 2021/day11/Task.java
+++ 2021/day11/Task.java
@@ -15,40 +15,42 @@ public class Task {
             var intLine = Arrays.stream(chars).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
             octopuses.add(intLine);
         }
+        List<List<Integer>> octopusesClone = new ArrayList<>();
+        octopuses.forEach(line -> octopusesClone.add(new ArrayList<>(line)));
 
-        System.out.println("Task1 solution: " + task1Solve(octopuses));
+        System.out.println("Task1 solution: " + taskSolve(octopuses, true));
+        System.out.println("Task2 solution: " + taskSolve(octopusesClone, false));
 
     }
 
 
-    private static int task1Solve(List<List<Integer>> octopuses) {
+    private static int taskSolve(List<List<Integer>> octopuses, Boolean isTask1) {
         int flashed = 0;
-        for (int step = 0; step < 2; step++) {
-
+        int maxStep = isTask1 ? 100 : 99999;
+        for (int step = 0; step < maxStep; step++) {
+            int flashedInStep = 0;
             for (List<Integer> octopusLine : octopuses) {
                 for (int i = 0; i < octopusLine.size(); i++) {
                     octopusLine.set(i, octopusLine.get(i) + 1);
                 }
             }
-            var changed = true;
+            var changed = 1;
             Set<String> set = new HashSet<>();
-            while (changed) {
+            while (changed != 0) {
+                changed--;
                 for (int i = 0; i < octopuses.size(); i++) {
                     for (int j = 0; j < octopuses.get(i).size(); j++) {
-                        changed = false;
-                        if (octopuses.get(i).get(j) > 9 && !set.contains(i+","+j)) {
-                            set.add(i+","+j);
-                            //System.out.println("current:" + i + ":" + j);
-                            for (int row = -1; row < 2; row++) {
-                                for (int column = -1; column < 2; column++) {
+                        if (octopuses.get(i).get(j) > 9 && !set.contains(i + "," + j)) {
+                            set.add(i + "," + j);
+                            for (int row = -1; row <= 1; row++) {
+                                for (int column = -1; column <= 1; column++) {
                                     if (!(row == 0 && column == 0)
-                                            && row + i != octopuses.size() && row + i >= 0
-                                            && column + j != octopuses.get(i).size() && column + j >= 0
+                                            && row + i < octopuses.size() && row + i >= 0
+                                            && column + j < octopuses.get(i).size() && column + j >= 0
                                     ) {
-                                        //System.out.println(row + i + ":" + (column + j));
                                         octopuses.get(i + row).set(j + column, octopuses.get(i + row).get(j + column) + 1);
-                                        if (octopuses.get(i).get(j) == 10) {
-                                            changed = true;
+                                        if (octopuses.get(i + row).get(j + column) == 10) {
+                                            changed++;
                                         }
                                     }
                                 }
@@ -62,11 +64,14 @@ public class Task {
                     if (octopuses.get(i).get(j) > 9) {
                         octopuses.get(i).set(j, 0);
                         flashed++;
+                        flashedInStep++;
+
                     }
                 }
             }
-            System.out.println("step "+ step);
-            octopuses.forEach(System.out::println);
+            if (!isTask1 && flashedInStep == 100) {
+                return step + 1;
+            }
         }
         return flashed;
     }
